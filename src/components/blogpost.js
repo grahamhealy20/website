1b67e19bf0123b6130e1de6a3c098399619723f3
Graham Healy <graham@pop-os.localdomain>
2020-07-27 23:38:49 +0100

    Prototyping home layout

---
+++ src/components/blogpost.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const BlogPost = () => (
+    <div>
+    <article className="blog-post">
+        <h3 className="title is-3">Blog post title</h3>
+        <h4 class="subtitle">3 days ago</h4>
+        <p>tempus elit at, iaculis elit. Curabitur fermentum turpis efficitur nulla facilisis viverra. Quisque congue ex non dolor tincidunt, id laoreet metus euismod. Nulla nibh risus, aliquet a lacus sed, ornare ornare turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.</p>
+    
+    </article>
+    </div>
+)
+
+export default BlogPost;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
 import React from "react"
 import Layout from "../components/layout"
+import About from '../components/about'
 import Sidebar from '../components/sidebar'
+import BlogPosts from "../components/blogposts"
 
 export default function Home() {
   return (
     <Layout>
       <div className="container-fluid">
-        <div className="columns is-gapless">
+        <div className="columns">
           <section className="sidebar column">
             <Sidebar></Sidebar>
           </section>
           <section className="about column is-two-thirds">
-            About
+            <About></About>
       </section>
           <section className="blog-posts column">
-            Posts
+            <BlogPosts></BlogPosts>
       </section>
         </div>
       </div>
