4e48dd80ccae9cbb6221c1e6aecd7beed6d890fc
Graham Healy <grahamh220@gmail.com>
2020-08-17 19:13:43 +0100

    Small responsive tweaks

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,10 +4,16 @@ import profilePhoto from "./profile.jpg"
 const Hero = () => (
     <div className="columns is-desktop is-vcentered">
         <div className="column is-two-thirds ">
-            <section className="section">
+            <section className="section about-me">
                 <h1 className="title hero-title">graham healy</h1>
                 <p>tempus elit at, iaculis elit. Curabitur fermentum turpis efficitur nulla facilisis viverra. Quisque congue ex non dolor tincidunt, id laoreet metus euismod. Nulla nibh risus, aliquet a lacus sed, ornare ornare turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer mi sem, iaculis eu nibh non, aliquet facilisis turpis. Sed non tortor ullamcorper, feugiat diam ac, pulvinar elit. Duis et elit non quam semper porttitor. In commodo finibus fringilla. Nunc ac risus et risus viverra malesuada. Sed eget commodo tellus. Quisque a nibh cursus, efficitur tortor sed, laoreet diam.</p>
+                <section className="contact-me">
+                    <a href="">Github</a>
+                    <a href="">Linkedin</a>
+                    <a href="">Blog</a>
+                </section>
             </section>
+
         </div>
         <div className="profile-image column">
             <div className="section">
